d5b150e460fb28d753468eaf5d9d7d8812c94ab0
Nzybwzy <781746210@qq.com>
2017-03-08 09:27:33 +0800

    测试map及添加简单跳转功能

--- app/components/Tab2.js
+++ app/components/Tab2.js
@@ -1,4 +1,12 @@
 import React, {Component} from "react"
+const ownerType = [
+        {k:"0",v: "全部"},
+        {k:"1",v: "业主"},
+        {k:"5",v: "住户"},
+        {k:"6",v: "租户"},
+        {k:"2",v: "商户"},
+        {k:"8",v: "其他"}
+    ]
 class Tab2 extends React.Component {
 	constructor(props) {
 		super(props);
@@ -7,24 +15,34 @@ class Tab2 extends React.Component {
 			pageSize: 10,
 			totalSize: 0
 		}
-
 	}
 	onHandleClickButton (tabName) {
 		alert(tabName)
 	}
-	setActivedTAb () {console.log(1)}
+	setActivedTAb (tab) {
+		alert(tab)
+	}
 	render() {
 		let { pageIndex } = this.state;
 		return(
 			<div>
-				<Button onHandleClickButton = {this.onHandleClickButton} />
-				<button onClick={() => this.setActivedTAb()}>按钮1</button>
+				<Button 
+					onHandleClickButton = {this.onHandleClickButton}
+					ownerType = {ownerType}
+					/>
+				<button onClick={() => this.setActivedTAb("按钮1")}>按钮1</button>
 			</div>
 		)
 	}
 }
-const Button = ({onHandleClickButton}) => 
+const Button = ({onHandleClickButton, ownerType}) => 
 	<div>
-		<button onClick={() => onHandleClickButton("sas")}>按钮</button>
+		{
+			ownerType && ownerType.map((item, index) => {
+				return (
+					<button key={index} onClick={() => onHandleClickButton(item.v)}>{item.v}</button>
+				)
+			})
+		}
 	</div>
 module.exports = Tab2;
--- app/main.js
+++ app/main.js
@@ -3,17 +3,86 @@ import ReactDOM from 'react-dom';
 import Tab from "./components/Tab";
 import Tab1 from "./components/Tab1";
 import Tab2 from "./components/Tab2";
+
+const ownerList = [
+        {k:"0",v: "全部全部全部全部"},
+        {k:"1",v: "业主业主业主业主"},
+        {k:"5",v: "住户住户住户住户"},
+        {k:"6",v: "租户租户租户租户"},
+        {k:"2",v: "商户商户商户商户"},
+        {k:"8",v: "其他其他其他其他"}
+    ]
 export default class AppComponent extends Component {
-  render() {
-    return (
-    <div>
-      	<Tab />
-      	<Tab1 />
-      	<Tab2 />
-    </div>
-      
-    );
-  }
+    constructor (props) {
+        super(props);
+        this.state = {
+            routers: "HomePage",
+            detail: ""
+        }
+    }
+    // 跳转列表页
+    onHandleGoList() {
+        this.setState({
+            routers: "ListPage"
+        })
+    }
+    // 跳转详情页
+    onHandleClickGoDetail(detail) {
+        this.setState({
+            routers: "DetailPage",
+            detail: detail
+        })
+    }
+    // 返回首页
+    onHandleClickGoBackToHome() {
+        this.setState({
+            routers: "HomePage"
+        })
+    }
+    // 详情返回列表页
+    onHandleClickGoBackToList() {
+        this.setState({
+            routers: "ListPage"
+        })
+    }
+    render() {
+        let { routers, detail } = this.state
+        // console.log(routers)
+        return (
+            <div>
+            {
+                routers == "HomePage" ? 
+                    <div>
+                        <Tab />
+                        <Tab1 />
+                        <Tab2 />
+                        <button style={{width: "200px", height: "200px", fontSize: "30px"}} onClick={() => {this.onHandleGoList()}}>列表页(点我)</button>
+                    </div> : 
+                routers == "ListPage" ? 
+                    <div>
+                        <button onClick={() => {this.onHandleClickGoBackToHome()}}>返回</button>
+                        <h1>列表页</h1>
+                        <ul>
+                            {
+                                ownerList.map((item, index) => {
+                                    return (
+                                        <li style={{cursor: "pointer"}} onClick={() => this.onHandleClickGoDetail(item.v)} key={index}>{item.v}(点我)</li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div> : 
+                routers == "DetailPage" ? 
+                    <div>
+                        <button onClick={() => {this.onHandleClickGoBackToList()}}>返回</button>
+                        <h1>详情</h1>
+                        <div>{detail}</div>
+                    </div>
+                    : null
+            }
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<AppComponent />, document.getElementById('content'));
